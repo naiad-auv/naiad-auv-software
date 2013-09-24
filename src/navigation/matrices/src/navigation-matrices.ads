41f0a0b6272436f3344e6e53fffe75ec8f7f6beb
Per-Erik Måhl <pml09001@student.mdh.se>
2013-09-24 11:29:11 +0200

    started on matrices package

---
+++ src/navigation/matrices/src/navigation-matrices.ads
@@ -0,0 +1,13 @@
+with Navigation;
+
+package Navigation.Matrices is
+   type CMatrix is tagged private;
+   type pCMatrix is access CMatrix;
+
+private
+   type CMatrix is tagged
+      record
+
+      end record;
+
+end Navigation.Matrices;
