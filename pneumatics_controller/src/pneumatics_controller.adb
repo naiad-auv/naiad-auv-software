0d134368e51e4e107f8d3d8a6615dbfa784c0194
kks <ntopoulos@yahoo.com>
2013-10-01 10:11:28 +0200

    Added *.exe to gitignore and minor code restructure.

--- pneumatics_controller/src/pneumatics_controller.adb
+++ pneumatics_controller/src/pneumatics_controller.adb
@@ -1,12 +1,15 @@
--- kks
 
+-- Written by: Konstantinos Konstantopoulos for the Naiad AUV project
+-- Last changed (yyyy-mm-dd): 2013-09-30
+
+with AVR.AT90CAN128.CALENDAR;
+with AT90CAN.CAN;
+with CAN_Handler;
 with Digital_IO;
 with Interfaces;
-with AVR.AT90CAN128.CALENDAR;
-with CAN_Handler;
-with AT90CAN.CAN;
 
 package body Pneumatics_Controller is
+
    procedure Init is
    begin
       Digital_IO.Make_Output_Pin(PIN_TORPEDO_LEFT);
@@ -17,20 +20,20 @@ package body Pneumatics_Controller is
       Digital_IO.Make_Output_Pin(PIN_GRIPPER_ROTATE);
    end Init;
 
-   procedure Actuate (u8Pin : in Interfaces.Unsigned_8; dDuration : in AVR.AT90CAN128.CALENDAR.Duration; bSimModeActive : in Boolean) is
+   procedure Actuate (u8Pin : in Interfaces.Unsigned_8; dT : in AVR.AT90CAN128.CALENDAR.Duration; bSimModeFlag : in Boolean) is
    begin
-      if not bSimModeActive then
+      if not bSimModeFlag then
          Digital_IO.Set_Pin(u8Pin);
-         AVR.AT90CAN128.CALENDAR.Delay_ms(dDuration);
+         AVR.AT90CAN128.CALENDAR.Delay_ms(dT);
          Digital_IO.Clear_Pin(u8Pin);
       else -- Simulate actuation duration.
-         AVR.AT90CAN128.CALENDAR.Delay_ms(dDuration);
+         AVR.AT90CAN128.CALENDAR.Delay_ms(dT);
       end if;
    end;
 
-   procedure Actuate (u8Pin : in Interfaces.Unsigned_8; bSetPin : in Boolean; bSimModeActive : in Boolean) is
+   procedure Actuate (u8Pin : in Interfaces.Unsigned_8; bSetPin : in Boolean; bSimModeFlag : in Boolean) is
    begin
-      if not bSimModeActive then
+      if not bSimModeFlag then
          if bSetPin then
             Digital_IO.Set_Pin(u8Pin);
          else
@@ -42,31 +45,31 @@ package body Pneumatics_Controller is
    end;
 
    procedure ISR (canMsg : in CAN_Handler.CAN_Message) is
-      bKillSwitch	: Boolean			:= False;
-      bSimModeActive		: Boolean			:= False;
+      bKillSwitchFlag	: Boolean := False;
+      bSimModeFlag	: Boolean := False;
    begin
 
       -- TODO: Read bKillSwitch
       -- TODO: Read bSimSwitch
 
-      if bKillSwitch = False then
+      if bKillSwitchFlag = False then
          case 0 is -- TODO: Msg.Data(0) is
             when 1 =>
-               Actuate(PIN_TORPEDO_LEFT, TORPEDO_ACTUATION_T, bSimModeActive);
+               Actuate(PIN_TORPEDO_LEFT, T_TORPEDO_ACTUATION, bSimModeFlag);
             when 2 =>
-               Actuate(PIN_TORPEDO_RIGHT, TORPEDO_ACTUATION_T, bSimModeActive);
+               Actuate(PIN_TORPEDO_RIGHT, T_TORPEDO_ACTUATION, bSimModeFlag);
             when 3 =>
-               Actuate(PIN_MARKER_LEFT, MARKER_ACTUATION_T, bSimModeActive);
+               Actuate(PIN_MARKER_LEFT, T_MARKER_ACTUATION, bSimModeFlag);
             when 4 =>
-               Actuate(PIN_MARKER_RIGHT, MARKER_ACTUATION_T, bSimModeActive);
+               Actuate(PIN_MARKER_RIGHT, T_MARKER_ACTUATION, bSimModeFlag);
             when 5 =>
-               Actuate(PIN_GRIPPER_GRAB, True, bSimModeActive);
+               Actuate(PIN_GRIPPER_GRAB, True, bSimModeFlag);
             when 6 =>
-               Actuate(PIN_GRIPPER_GRAB, False, bSimModeActive);
+               Actuate(PIN_GRIPPER_GRAB, False, bSimModeFlag);
             when 7 =>
-               Actuate(PIN_GRIPPER_ROTATE, True, bSimModeActive);
+               Actuate(PIN_GRIPPER_ROTATE, True, bSimModeFlag);
             when 8 =>
-               Actuate(PIN_GRIPPER_ROTATE, False, bSimModeActive);
+               Actuate(PIN_GRIPPER_ROTATE, False, bSimModeFlag);
             when others =>
                null;
             end case;
@@ -76,4 +79,5 @@ package body Pneumatics_Controller is
          null; -- TODO: SEND MSG 'KILLED'
       end if;
    end ISR;
+
 end Pneumatics_Controller;
--- pneumatics_controller/src/pneumatics_controller.ads
+++ pneumatics_controller/src/pneumatics_controller.ads
@@ -1,10 +1,13 @@
--- kks
 
-with Interfaces;
-with CAN_Handler;
+-- Written by: Konstantinos Konstantopoulos for the Naiad AUV project
+-- Last changed (yyyy-mm-dd): 2013-09-30
+
 with AVR.AT90CAN128.CALENDAR;
+with CAN_Handler;
+with Interfaces;
 
 package Pneumatics_Controller is
+
    PIN_TORPEDO_LEFT	: Constant Interfaces.Unsigned_8 := 0;
    PIN_TORPEDO_RIGHT	: Constant Interfaces.Unsigned_8 := 0;
    PIN_MARKER_LEFT	: Constant Interfaces.Unsigned_8 := 0;
@@ -12,9 +15,10 @@ package Pneumatics_Controller is
    PIN_GRIPPER_GRAB	: Constant Interfaces.Unsigned_8 := 0;
    PIN_GRIPPER_ROTATE	: Constant Interfaces.Unsigned_8 := 0;
 
-   TORPEDO_ACTUATION_T : Constant AVR.AT90CAN128.CALENDAR.Duration := 1000;
-   MARKER_ACTUATION_T : Constant AVR.AT90CAN128.CALENDAR.Duration := 1000;
+   T_TORPEDO_ACTUATION : Constant AVR.AT90CAN128.CALENDAR.Duration := 1000;
+   T_MARKER_ACTUATION : Constant AVR.AT90CAN128.CALENDAR.Duration := 1000;
 
    procedure Init;
    procedure ISR(canMsg : in CAN_Handler.CAN_Message);
+
 end Pneumatics_Controller;
