0da1d9d2d95e940b5379fbf6750c80e88250ff61
Per-Erik Måhl <pml09001@student.mdh.se>
2013-10-02 07:36:59 +0200

    updated matrices

--- src/math/matrices/src/math-matrices.adb
+++ src/math/matrices/src/math-matrices.adb
@@ -1,9 +1,46 @@
 with Math;
 
 package body Math.Matrices is
-   function Dummy return boolean is
+
+   function pxCreate_From_Quaternion (pxFromQuaternion : in Math.Quaternions.pCQuaternion) return pCMatrix is
+      pxNewMatrix : pCMatrix;
    begin
-      return true;
-   end Dummy;
+      pxNewMatrix := new CMatrix;
+
+      -- first row
+      -- 1-2y2-2z2
+      pxNewMatrix.fAA := 1.0 - (2.0 * pxFromQuaternion.fGet_Y * pxFromQuaternion.fGet_Y) - (2.0 * pxFromQuaternion.fGet_Z * pxFromQuaternion.fGet_Z);
+
+      -- 2xy-2wz
+      pxNewMatrix.fAB := (2.0 * pxFromQuaternion.fGet_X * pxFromQuaternion.fGet_Y) - (2.0 * pxFromQuaternion.fGet_W * pxFromQuaternion.fGet_Z);
+
+      -- 2xz+2wy
+      pxNewMatrix.fAC := (2.0 * pxFromQuaternion.fGet_X * pxFromQuaternion.fGet_Z) + (2.0 * pxFromQuaternion.fGet_W * pxFromQuaternion.fGet_Y);
+
+
+      -- second row
+      -- 2xy+2wz
+      pxNewMatrix.fBA := (2.0 * pxFromQuaternion.fGet_X + pxFromQuaternion.fGet_Y) + (2.0 * pxFromQuaternion.fGet_W * pxFromQuaternion.fGet_Z);
+
+      -- 1-2x2-2z2
+      pxNewMatrix.fBB := 1.0 - (2.0 * pxFromQuaternion.fGet_X * pxFromQuaternion.fGet_X) - (2.0 * pxFromQuaternion.fGet_Z * pxFromQuaternion.fGet_Z);
+
+      -- 2yz+2wx
+      pxNewMatrix.fBC := (2.0 * pxFromQuaternion.fGet_Y * pxFromQuaternion.fGet_Z) + (2.0 * pxFromQuaternion.fGet_W * pxFromQuaternion.fGet_X);
+
+
+      -- third row
+      -- 2xz-2wy
+      pxNewMatrix.fCA := (2.0 * pxFromQuaternion.fGet_X * pxFromQuaternion.fGet_Z) - (2.0 * pxFromQuaternion.fGet_W * pxFromQuaternion.fGet_Y);
+
+      -- 2yz-2wx
+      pxNewMatrix.fCB := (2.0 * pxFromQuaternion.fGet_Y * pxFromQuaternion.fGet_Z) - (2.0 * pxFromQuaternion.fGet_W * pxFromQuaternion.fGet_X);
+
+      -- 1-2x2-2y2
+      pxNewMatrix.fCC := 1.0 - (2.0 * pxFromQuaternion.fGet_X * pxFromQuaternion.fGet_X) - (2.0 * pxFromQuaternion.fGet_Y * pxFromQuaternion.fGet_Y);
+
+      return pxNewMatrix;
+   end pxCreate_From_Quaternion;
+
 
 end Math.Matrices;
--- src/math/matrices/src/math-matrices.ads
+++ src/math/matrices/src/math-matrices.ads
@@ -1,15 +1,25 @@
 with Math;
+with Math.Quaternions;
+
 
 package Math.Matrices is
    type CMatrix is tagged private;
    type pCMatrix is access CMatrix;
 
-   function Dummy return boolean;
+   function pxCreate_From_Quaternion (pxFromQuaternion : in Math.Quaternions.pCQuaternion) return pCMatrix;
 
 private
    type CMatrix is tagged
       record
-	null;
+         fAA : float;
+         fAB : float;
+         fAC : float;
+         fBA : float;
+         fBB : float;
+         fBC : float;
+         fCA : float;
+         fCB : float;
+         fCC : float;
       end record;
 
 end Math.Matrices;
