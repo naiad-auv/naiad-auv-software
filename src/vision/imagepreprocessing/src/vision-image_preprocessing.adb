4f4ef83632352a1422c91f7ed4d06105f8b1e2b5
debajyoti <dave0908@gmail.com>
2013-10-07 08:23:38 +0200

    fixed comments

--- src/vision/imagepreprocessing/src/vision-image_preprocessing.adb
+++ src/vision/imagepreprocessing/src/vision-image_preprocessing.adb
@@ -1,7 +1,8 @@
 package body Vision.Image_Preprocessing is
-   --function color to greyscale
    function DummyTest return Boolean is
    begin
       return True;
    end DummyTest;
+
+
 end Vision.Image_Preprocessing;
--- src/vision/imagepreprocessing/src/vision-image_preprocessing.ads
+++ src/vision/imagepreprocessing/src/vision-image_preprocessing.ads
@@ -1,4 +1,3 @@
 package Vision.Image_Preprocessing is
-   --function color to greyscale
    function DummyTest return Boolean;
 end Vision.Image_Preprocessing;
