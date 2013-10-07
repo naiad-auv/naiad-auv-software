be4f3b614376c58d4c796cd88b103bca1b6606c2
vision <vision@visionbox.(none)>
2013-10-07 14:53:26 +0200

    removed canny

--- src/vision/imageprocessing/canny/src/vision-image_processing-canny.ads
+++
@@ -1,5 +0,0 @@
-package Vision.Image_Processing.Canny is
-   --function color to greyscale
-   function DummyTest return Boolean;
-   function Canny return Boolean;
-end Vision.Image_Processing.Canny;
