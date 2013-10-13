3ee689a1751f34a22fe3fb18c54fa28e8dfe3cca
Gerard Duff <GerardDuff@gmail.com>
2013-10-13 21:02:02 +0200

    cleaned up main, added hough circles procedure to the processing package

--- src/vision/imageprocessing/src/vision-image_processing.ads
+++ src/vision/imageprocessing/src/vision-image_processing.ads
@@ -2,6 +2,8 @@ package Vision.Image_Processing is
    function DummyTest return Boolean;
    procedure Convert_To_Greyscale(iImageSource, iImageDestination, iImageFilter: in integer);
    procedure Canny(iImageSource, iImageDestination, iLowThreshold, iHighThreshold, iKernelSize: in integer);
+   procedure Hough_Circles(iImageDestination, inverseRatioOfResolution, minDistBetweenCenters, houghCannyUpThres, centerDetectionThreshold, minRadius, maxRadius: in integer);
+   procedure Draw_Hough_Circles(iImageSource: in integer);
 
    --function cleanup
    --function histogram analysis
