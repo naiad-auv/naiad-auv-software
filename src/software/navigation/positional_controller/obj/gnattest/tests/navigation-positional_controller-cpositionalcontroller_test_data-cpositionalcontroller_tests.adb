c8c0473bc01c3abf4aea51c84b971e88193b5f55
EmiBe <emibe@ubuntu.(none)>
2013-10-21 12:39:01 +0200

    test updates

---
+++ src/software/navigation/positional_controller/obj/gnattest/tests/navigation-positional_controller-cpositionalcontroller_test_data-cpositionalcontroller_tests.adb
@@ -0,0 +1,101 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Navigation.Positional_Controller.CPositionalController_Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Navigation.Positional_Controller.CPositionalController_Test_Data.CPositionalController_Tests is
+
+
+--  begin read only
+   procedure Test_xGet_Positional_Thruster_Control_Values (Gnattest_T : in out Test_CPositionalController);
+   procedure Test_xGet_Positional_Thruster_Control_Values_1d7d6d (Gnattest_T : in out Test_CPositionalController) renames Test_xGet_Positional_Thruster_Control_Values;
+--  id:2.1/1d7d6de9f88b64a2/xGet_Positional_Thruster_Control_Values/1/0/
+   procedure Test_xGet_Positional_Thruster_Control_Values (Gnattest_T : in out Test_CPositionalController) is
+   --  navigation-positional_controller.ads:14:4:xGet_Positional_Thruster_Control_Values
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+      use Navigation.Motion_Component;
+
+      pxPositionalController : Navigation.Positional_Controller.pCPositionalController;
+
+      pxNewWanted : Math.Vectors.pCVector := Math.Vectors.pxCreate(5.0,5.0,5.0);
+
+      pxCurrentPosition : Math.Vectors.pCVector := Math.Vectors.pxCreate(0.0,0.0,0.0);
+      pxCurrentOrientation : Math.Matrices.pCMatrix := Math.Matrices.pxCreate_Identity;
+
+      pxPositionalScalings : Navigation.PID_Controller.TPIDComponentScalings := (1.0,1.0,1.0);
+
+
+      xPositionalControlValues : Navigation.Thrusters.TThrusterEffects;
+   begin
+
+      pxPositionalController := Navigation.Positional_Controller.pxCreate(pxCurrentAbsolutePosition    => pxCurrentPosition,
+                                                                          pxWantedAbsolutePosition     => pxNewWanted,
+                                                                          pxCurrentAbsoluteOrientation => pxCurrentOrientation);
+
+      pxPositionalController.Set_New_PID_Component_Scalings(Navigation.Motion_Component.AllComponents, pxPositionalScalings);
+
+      pxPositionalController.Update_Current_Errors;
+
+      xPositionalControlValues := pxPositionalController.xGet_Positional_Thruster_Control_Values(1.0);
+
+      AUnit.Assertions.Assert(Condition => abs(xPositionalControlValues(Thrusters.XPosition)) > 0.00001,
+                              Message => "PositionalControlvalue component 1 is 0");
+
+      AUnit.Assertions.Assert(Condition => abs(xPositionalControlValues(Thrusters.YPosition)) > 0.00001,
+                              Message => "PositionalControlvalue component 1 is 0");
+
+      AUnit.Assertions.Assert(Condition => abs(xPositionalControlValues(Thrusters.ZPosition)) > 0.00001,
+                              Message => "PositionalControlvalue component 1 is 0");
+
+
+--  begin read only
+   end Test_xGet_Positional_Thruster_Control_Values;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Update_Current_Errors (Gnattest_T : in out Test_CPositionalController);
+   procedure Test_Update_Current_Errors_db8191 (Gnattest_T : in out Test_CPositionalController) renames Test_Update_Current_Errors;
+--  id:2.1/db81912ffacc7cfb/Update_Current_Errors/1/0/
+   procedure Test_Update_Current_Errors (Gnattest_T : in out Test_CPositionalController) is
+   --  navigation-positional_controller.ads:16:4:Update_Current_Errors
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+	--TODO måste ha mocking för att kunna testa
+--  begin read only
+      null;
+   end Test_Update_Current_Errors;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Set_New_PID_Component_Scalings (Gnattest_T : in out Test_CPositionalController);
+   procedure Test_Set_New_PID_Component_Scalings_90f3b2 (Gnattest_T : in out Test_CPositionalController) renames Test_Set_New_PID_Component_Scalings;
+--  id:2.1/90f3b2739b4fefa7/Set_New_PID_Component_Scalings/1/0/
+   procedure Test_Set_New_PID_Component_Scalings (Gnattest_T : in out Test_CPositionalController) is
+   --  navigation-positional_controller.ads:19:4:Set_New_PID_Component_Scalings
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      --TODO måste ha mocking för att kunna testa
+
+--  begin read only
+      null;
+   end Test_Set_New_PID_Component_Scalings;
+--  end read only
+
+end Navigation.Positional_Controller.CPositionalController_Test_Data.CPositionalController_Tests;
---
+++ src/software/navigation/thruster_configurator/obj/gnattest/tests/navigation-thruster_configurator-cthrusterconfigurator_test_data-cthrusterconfigurator_tests.adb
@@ -0,0 +1,96 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Navigation.Thruster_Configurator.CThrusterConfigurator_Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Navigation.Thruster_Configurator.CThrusterConfigurator_Test_Data.CThrusterConfigurator_Tests is
+
+
+--  begin read only
+   procedure Test_iGet_Number_Of_Thrusters (Gnattest_T : in out Test_CThrusterConfigurator);
+   procedure Test_iGet_Number_Of_Thrusters_d77f18 (Gnattest_T : in out Test_CThrusterConfigurator) renames Test_iGet_Number_Of_Thrusters;
+--  id:2.1/d77f18df25a8a493/iGet_Number_Of_Thrusters/1/0/
+   procedure Test_iGet_Number_Of_Thrusters (Gnattest_T : in out Test_CThrusterConfigurator) is
+   --  navigation-thruster_configurator.ads:22:4:iGet_Number_Of_Thrusters
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_iGet_Number_Of_Thrusters;
+--  end read only
+
+
+--  begin read only
+   procedure Test_tfGet_Thruster_Effects_Matrix (Gnattest_T : in out Test_CThrusterConfigurator);
+   procedure Test_tfGet_Thruster_Effects_Matrix_972577 (Gnattest_T : in out Test_CThrusterConfigurator) renames Test_tfGet_Thruster_Effects_Matrix;
+--  id:2.1/9725779a9fcce1b2/tfGet_Thruster_Effects_Matrix/1/0/
+   procedure Test_tfGet_Thruster_Effects_Matrix (Gnattest_T : in out Test_CThrusterConfigurator) is
+   --  navigation-thruster_configurator.ads:23:4:tfGet_Thruster_Effects_Matrix
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_tfGet_Thruster_Effects_Matrix;
+--  end read only
+
+
+--  begin read only
+   procedure Test_tfGet_Thruster_Values (Gnattest_T : in out Test_CThrusterConfigurator);
+   procedure Test_tfGet_Thruster_Values_fd2a06 (Gnattest_T : in out Test_CThrusterConfigurator) renames Test_tfGet_Thruster_Values;
+--  id:2.1/fd2a061725e8db0d/tfGet_Thruster_Values/1/0/
+   procedure Test_tfGet_Thruster_Values (Gnattest_T : in out Test_CThrusterConfigurator) is
+   --  navigation-thruster_configurator.ads:25:4:tfGet_Thruster_Values
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_tfGet_Thruster_Values;
+--  end read only
+
+
+--  begin read only
+   procedure Test_tfCreate_Extended_Matrix (Gnattest_T : in out Test_CThrusterConfigurator);
+   procedure Test_tfCreate_Extended_Matrix_b5b96a (Gnattest_T : in out Test_CThrusterConfigurator) renames Test_tfCreate_Extended_Matrix;
+--  id:2.1/b5b96a17803e5ede/tfCreate_Extended_Matrix/1/0/
+   procedure Test_tfCreate_Extended_Matrix (Gnattest_T : in out Test_CThrusterConfigurator) is
+   --  navigation-thruster_configurator.ads:37:4:tfCreate_Extended_Matrix
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_tfCreate_Extended_Matrix;
+--  end read only
+
+end Navigation.Thruster_Configurator.CThrusterConfigurator_Test_Data.CThrusterConfigurator_Tests;
