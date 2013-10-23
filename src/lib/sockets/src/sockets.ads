fc5489b67f77b6c7a9e1a67521ae7096090aedc2
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-10-23 11:21:00 +0200

    Added two types within sockets library.

--- src/lib/sockets/src/sockets.ads
+++ src/lib/sockets/src/sockets.ads
@@ -1,4 +1,17 @@
 package Sockets is
+
+    type Socket_Type is tagged
+        record
+            Address : String(1 .. 255);
+        end record;
+
+    subtype Port_Type is Positive range 1 .. 65535;
+
+    type Datagram_Socket_Type is new Socket_Type with
+        record
+            Port : Port_Type;
+        end record;
+
     procedure Send_UDP_Message;
     procedure Listen_On_Socket;
 end Sockets;
