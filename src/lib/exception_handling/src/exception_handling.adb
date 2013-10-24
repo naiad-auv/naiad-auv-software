5ab99980d7db8ac98766e9e1c986458fba0e2258
Per-Erik Måhl <pml09001@student.mdh.se>
2013-10-24 11:10:15 +0200

    exception handling lib done

--- src/lib/exception_handling/src/exception_handling.adb
+++ src/lib/exception_handling/src/exception_handling.adb
@@ -1,26 +1,24 @@
+with Ada.Text_IO;
+with GNAT.Traceback;
+with GNAT.Traceback.Symbolic;
+
 package body Exception_Handling is
 
-   --------------
-   -- Get_Name --
-   --------------
-
-   function Get_Name (E : in TException) return string is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Get_Name unimplemented");
-      raise Program_Error;
-      return Get_Name (E);
-   end Get_Name;
 
    -------------------------
    -- Unhandled_Exception --
    -------------------------
 
-   procedure Unhandled_Exception (E : in TException) is
+   procedure Unhandled_Exception (E : in Ada.Exceptions.Exception_Occurrence) is
+      Trace  : GNAT.Traceback.Tracebacks_Array (1..1_000);
+      Length : Natural;
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Unhandled_Exception unimplemented");
-      raise Program_Error;
+      Ada.Text_IO.Put_Line("Unhandled exception: " & Ada.Exceptions.Exception_Name(E));
+      Ada.Text_IO.Put_Line("Call stack:");
+      GNAT.Traceback.Call_Chain (Trace, Length);
+      Ada.Text_IO.Put_Line (GNAT.Traceback.Symbolic.Symbolic_Traceback (Trace (1..Length)));
+      raise UnhandledException;
    end Unhandled_Exception;
+
 
 end Exception_Handling;
--- src/lib/exception_handling/src/exception_handling.ads
+++ src/lib/exception_handling/src/exception_handling.ads
@@ -2,19 +2,16 @@ with Ada.Exceptions;
 
 package Exception_Handling is
 
-   type EExceptions is (Unhandled_Exception,
-                        Null_Pointer,
-                        Not_Unit_Quaternion,
-                        Singular_Matrix,
-                        Parallel_Planes,
-                        Division_By_Zero,
-                        Index_Out_Of_Bounds);
 
-   type TException is new EExceptions;
-
+   UnhandledException : exception;
+   NullPointer : exception;
+   NotUnitQuaternion : exception;
+   SingularMatrix : exception;
+   ParallelPlanes : exception;
+   DivisionByZero : exception;
+   IndexOutOfBounds : exception;
 
 
-   function Get_Name(E : in TException) return string;
-   procedure Unhandled_Exception (E : in TException);
+   procedure Unhandled_Exception (E : in Ada.Exceptions.Exception_Occurrence);
 
 end Exception_Handling;
