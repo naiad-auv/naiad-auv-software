aea5415f6cfd4f6aaaac2215b1df1bd266658756
Per-Erik Måhl <pml09001@student.mdh.se>
2013-10-24 08:58:23 +0200

    src docs

---
+++ documentation/src_doc/src/main.adb
@@ -0,0 +1,24 @@
+with Math;
+with Math.Angles;
+with Math.Matrices;
+with Math.Planes;
+with Math.Quaternions;
+with Math.Vectors;
+
+with Navigation;
+with Navigation.Dispatcher;
+with Navigation.Drift_Controller;
+with Navigation.Motion_Component;
+with Navigation.Motion_Controller;
+with Navigation.Orientational_Controller;
+with Navigation.PID_Controller;
+with Navigation.Positional_Controller;
+with Navigation.Thruster_Configurator;
+with Navigation.Thrusters;
+
+procedure Main is
+
+begin
+   --  Insert code here.
+   null;
+end Main;
