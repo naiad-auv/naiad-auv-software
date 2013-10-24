50247354850b938339e5ce688ae1e510377260cc
EmiBe <emibe@ubuntu.(none)>
2013-10-24 08:29:30 +0200

    updated PID test

--- src/software/navigation/pid_controller/obj/gnattest/tests/navigation-pid_controller-cpid_controller_test_data-cpid_controller_tests.adb
+++ src/software/navigation/pid_controller/obj/gnattest/tests/navigation-pid_controller-cpid_controller_test_data-cpid_controller_tests.adb
@@ -6,17 +6,18 @@
 --  placed into Navigation.PID_Controller.CPID_Controller_Test_Data.
 
 with AUnit.Assertions; use AUnit.Assertions;
+with Ada.Text_IO;
 
 package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller_Tests is
 
 
---  begin read only
+   --  begin read only
    procedure Test_Set_New_Set_Point (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_Set_New_Set_Point_f8f7bc (Gnattest_T : in out Test_CPID_Controller) renames Test_Set_New_Set_Point;
---  id:2.1/f8f7bc0264746f6e/Set_New_Set_Point/1/0/
+   --  id:2.1/f8f7bc0264746f6e/Set_New_Set_Point/1/0/
    procedure Test_Set_New_Set_Point (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:20:4:Set_New_Set_Point
---  end read only
+      --  navigation-pid_controller.ads:20:4:Set_New_Set_Point
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -33,18 +34,18 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
 
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
---  begin read only
+      --  begin read only
    end Test_Set_New_Set_Point;
---  end read only
+   --  end read only
 
 
---  begin read only
+   --  begin read only
    procedure Test_Set_New_PID_Component_Scalings (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_Set_New_PID_Component_Scalings_c67cc5 (Gnattest_T : in out Test_CPID_Controller) renames Test_Set_New_PID_Component_Scalings;
---  id:2.1/c67cc5a44aa5cde6/Set_New_PID_Component_Scalings/1/0/
+   --  id:2.1/c67cc5a44aa5cde6/Set_New_PID_Component_Scalings/1/0/
    procedure Test_Set_New_PID_Component_Scalings (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:22:4:Set_New_PID_Component_Scalings
---  end read only
+      --  navigation-pid_controller.ads:22:4:Set_New_PID_Component_Scalings
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -64,18 +65,18 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
                                                                                                         & float'Image(pxObject.fDerivativeScale)));
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
---  begin read only
+      --  begin read only
    end Test_Set_New_PID_Component_Scalings;
---  end read only
+   --  end read only
 
 
---  begin read only
+   --  begin read only
    procedure Test_Update_Current_Value_From_External_Source (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_Update_Current_Value_From_External_Source_32a416 (Gnattest_T : in out Test_CPID_Controller) renames Test_Update_Current_Value_From_External_Source;
---  id:2.1/32a416056ee168b1/Update_Current_Value_From_External_Source/1/0/
+   --  id:2.1/32a416056ee168b1/Update_Current_Value_From_External_Source/1/0/
    procedure Test_Update_Current_Value_From_External_Source (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:24:4:Update_Current_Value_From_External_Source
---  end read only
+      --  navigation-pid_controller.ads:24:4:Update_Current_Value_From_External_Source
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -90,18 +91,18 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
                               Message   => "fCurrentValue is not updated as it should");
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
---  begin read only
+      --  begin read only
    end Test_Update_Current_Value_From_External_Source;
---  end read only
+   --  end read only
 
 
---  begin read only
+   --  begin read only
    procedure Test_Reset_Controller (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_Reset_Controller_033697 (Gnattest_T : in out Test_CPID_Controller) renames Test_Reset_Controller;
---  id:2.1/0336972d1df31fa2/Reset_Controller/1/0/
+   --  id:2.1/0336972d1df31fa2/Reset_Controller/1/0/
    procedure Test_Reset_Controller (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:26:4:Reset_Controller
---  end read only
+      --  navigation-pid_controller.ads:26:4:Reset_Controller
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -138,18 +139,18 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
                               Message => "Proportional scale did not reset");
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
---  begin read only
+      --  begin read only
    end Test_Reset_Controller;
---  end read only
+   --  end read only
 
 
---  begin read only
+   --  begin read only
    procedure Test_xGet_New_Control_Value (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_xGet_New_Control_Value_4592b3 (Gnattest_T : in out Test_CPID_Controller) renames Test_xGet_New_Control_Value;
---  id:2.1/4592b3448a9fc884/xGet_New_Control_Value/1/0/
+   --  id:2.1/4592b3448a9fc884/xGet_New_Control_Value/1/0/
    procedure Test_xGet_New_Control_Value (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:28:4:xGet_New_Control_Value
---  end read only
+      --  navigation-pid_controller.ads:28:4:xGet_New_Control_Value
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -171,9 +172,9 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
       end loop;
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
---  begin read only
+      --  begin read only
    end Test_xGet_New_Control_Value;
---  end read only
+   --  end read only
 
 
    procedure Test_xGet_New_Control_Value_Increasing_Since_No_Decrease_In_Error(Gnattest_T : in out Test_CPID_Controller) is
@@ -203,7 +204,7 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
    procedure Test_xGet_New_Control_Value_Decreasing_Since_Overshoot(Gnattest_T : in out Test_CPID_Controller) is
       pxObject : pCPIDController;
       value, lastValue : float := 0.0;
-      xScalings : Navigation.PID_Controller.TPIDComponentScalings := (0.001, 0.001, 0.001);
+      xScalings : Navigation.PID_Controller.TPIDComponentScalings := (0.001, 0.00001, 0.01);
 
    begin
       pxObject := PID_Controller.pxCreate;
@@ -215,31 +216,32 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
       for i in 1 .. 100 loop
          value := pxObject.xGet_New_Control_Value(1.0);
          AUnit.Assertions.Assert(Condition => Value > lastValue,
-                                 Message   => "Control value not increasing when error is not changing");
+				 Message   => "Control value not increasing when error is not changing");
          lastValue := value;
       end loop;
 
       pxObject.Update_Current_Value_From_External_Source(100.0);
 
-      for i in 1 .. 100 loop
          value := pxObject.xGet_New_Control_Value(1.0);
-         AUnit.Assertions.Assert(Condition => value <= lastValue,
+      for i in 1 .. 100 loop
+
+         value := pxObject.xGet_New_Control_Value(1.0);
+         AUnit.Assertions.Assert(Condition => abs(value - lastValue) <= 0.1,
                                  Message   => "Control value not decreasing when at setpoint");
          lastValue := value;
+
       end loop;
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
    end Test_xGet_New_Control_Value_Decreasing_Since_Overshoot;
 
-
-
---  begin read only
+   --  begin read only
    procedure Test_fGetIntergralScale (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_fGetIntergralScale_953a97 (Gnattest_T : in out Test_CPID_Controller) renames Test_fGetIntergralScale;
---  id:2.1/953a977f968a3c6e/fGetIntergralScale/1/0/
+   --  id:2.1/953a977f968a3c6e/fGetIntergralScale/1/0/
    procedure Test_fGetIntergralScale (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:30:4:fGetIntergralScale
---  end read only
+      --  navigation-pid_controller.ads:30:4:fGetIntergralScale
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -253,18 +255,18 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
                               Message   => "fGetIntegralScale does not return 2.0");
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
---  begin read only
+      --  begin read only
    end Test_fGetIntergralScale;
---  end read only
+   --  end read only
 
 
---  begin read only
+   --  begin read only
    procedure Test_fGetDerivativeScale (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_fGetDerivativeScale_53214f (Gnattest_T : in out Test_CPID_Controller) renames Test_fGetDerivativeScale;
---  id:2.1/53214f1e29637f2e/fGetDerivativeScale/1/0/
+   --  id:2.1/53214f1e29637f2e/fGetDerivativeScale/1/0/
    procedure Test_fGetDerivativeScale (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:32:4:fGetDerivativeScale
---  end read only
+      --  navigation-pid_controller.ads:32:4:fGetDerivativeScale
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
 
@@ -279,18 +281,18 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
 
 
---  begin read only
+      --  begin read only
    end Test_fGetDerivativeScale;
---  end read only
+   --  end read only
 
 
---  begin read only
+   --  begin read only
    procedure Test_fGetProportionalScale (Gnattest_T : in out Test_CPID_Controller);
    procedure Test_fGetProportionalScale_b0d874 (Gnattest_T : in out Test_CPID_Controller) renames Test_fGetProportionalScale;
---  id:2.1/b0d874ac60f47775/fGetProportionalScale/1/0/
+   --  id:2.1/b0d874ac60f47775/fGetProportionalScale/1/0/
    procedure Test_fGetProportionalScale (Gnattest_T : in out Test_CPID_Controller) is
-   --  navigation-pid_controller.ads:34:4:fGetProportionalScale
---  end read only
+      --  navigation-pid_controller.ads:34:4:fGetProportionalScale
+      --  end read only
 
       pragma Unreferenced (Gnattest_T);
       pxObject : pCPIDController := null;
@@ -302,8 +304,8 @@ package body Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller
       Aunit.Assertions.Assert(Condition => pxObject.fGetProportionalScale = 1.0,
                               Message   => "fGetProportionalScale did not return 1.0");
       Navigation.PID_Controller.Free(pxPIDControllerToDeallocate => pxObject);
---  begin read only
+      --  begin read only
    end Test_fGetProportionalScale;
---  end read only
+   --  end read only
 
 end Navigation.PID_Controller.CPID_Controller_Test_Data.CPID_Controller_Tests;
