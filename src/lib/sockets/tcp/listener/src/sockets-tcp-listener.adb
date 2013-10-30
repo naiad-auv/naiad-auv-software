a03308ebc2c4b501dfba8424af324f1d0ed2e592
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-10-30 08:53:57 +0100

    Added the Sockets.Tcp.Listener project.

---
+++ src/lib/sockets/tcp/listener/src/sockets-tcp-listener.adb
@@ -0,0 +1,9 @@
+package body Sockets.Tcp.Listener is
+
+   procedure Main is
+   begin
+        -- Insert code here.
+        null;
+    end Main;
+
+end Sockets.Tcp.Listener;
---
+++ src/lib/sockets/tcp/listener/src/sockets-tcp-listener.ads
@@ -0,0 +1,5 @@
+package Sockets.Tcp.Listener is
+
+    procedure Main;
+
+end Sockets.Tcp.Listener;
