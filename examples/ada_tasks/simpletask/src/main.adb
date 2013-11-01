973167c6b9cdd57919e166fa96c4af88198f676d
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-11-01 10:53:26 +0100

    Removed Text_IO parts.

--- examples/ada_tasks/simpletask/src/main.adb
+++ examples/ada_tasks/simpletask/src/main.adb
@@ -1,19 +1,16 @@
 pragma Profile(Ravenscar);
 
-with Ada.Text_IO;
-
 procedure Main is
+
+    task type Task_One;
 
     task body Task_One is
         i: Integer := 1;
     begin
-        Ada.Text_IO.Put_Line("Task_One - Start");
         loop
-            Ada.Text_IO.Put_Line("Task_One - " & Integer'Image(i));
             i := 1 + i;
             exit when i = 10;
         end loop;
-        Ada.Text_IO.Put_Line("Task_One - End");
     end Task_One;
 
 begin
