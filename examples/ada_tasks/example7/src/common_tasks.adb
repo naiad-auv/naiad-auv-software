f3d7204f71db03728d2a908e6273bd0172f9a8ee
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-11-04 14:10:48 +0100

    Ade tasks example 7 now works with a simple loop between two tasks. Next step is to accept port number and ip address as input (Tcp_Sender_Type and Tcp_Listener_Type).

--- examples/ada_tasks/example7/src/common_tasks.adb
+++ examples/ada_tasks/example7/src/common_tasks.adb
@@ -1,8 +1,69 @@
 with Ada.Real_Time;
 with Ada.Text_IO;
 with GNAT.Sockets;
+with Ada.Streams;
 
 package body Common_Tasks is
+    CRLF : constant String := ASCII.CR & ASCII.LF;
+    PORT : constant GNAT.Sockets.Port_Type := 50000;
+    Tmp : Ada.Streams.Stream_Element_Array (1 .. 65535);
+    DST_ADDRESS : constant String := "127.0.0.1";
+    SRC_ADDRESS : constant String := "127.0.0.1";
+    LOCALHOST : constant String := "127.0.0.1";
+
+    ------------------
+    -- Read_Request --
+    ------------------
+
+    -- function Read_Request originally written by Simon Wright from
+    -- https://groups.google.com/forum/#!topic/comp.lang.ada/j23Fv-uvNX8
+    function Read_Request (From : GNAT.Sockets.Socket_Type) return Natural is
+        use Ada.Streams;
+        use Standard.ASCII;
+        Last : Ada.Streams.Stream_Element_Offset := Tmp'First - 1;
+        Next : Ada.Streams.Stream_Element_Offset;
+        Termination : constant Ada.Streams.Stream_Element_Array :=
+            (Character'Pos (CR),
+            Character'Pos (LF),
+            Character'Pos (CR),
+            Character'Pos (LF));
+        Socket_Stream_Access : GNAT.Sockets.Stream_Access := GNAT.Sockets.Stream (From);
+    begin
+    --  We need to read the whole request from the client. Of course
+    --  we don't know how long it is. We can't just issue an
+    --  Ada.Streams.Read for a large buffer, because the client may
+    --  not have sent that much and if she hasn't we'll block until
+    --  she gives up and closes the socket. So we read a character
+    --  at a time until we've got the CR/LF/CR/LF which terminates
+    --  the line.
+    loop
+        Ada.Streams.Read (Stream => Socket_Stream_Access.all,
+                        Item => Tmp (Last + 1 .. Last + 1),
+                        Last => Next);
+        exit when Next = Last;
+
+        -- Ada.Text_IO.Put(Tmp(Last));
+
+        Last := Last + 1;
+        exit when Last >= Termination'Length
+        and then Tmp (Last - 3 .. Last) = Termination;
+        exit when Last = Tmp'Last;
+    end loop;
+
+        --Free_Stream(S);
+        return Natural(Last);
+    end Read_Request;
+
+    -----------------
+    -- Get_Message --
+    -----------------
+    function Get_Message (N : Natural) return String is
+        Result : String (1 .. N);
+        for Result'Address use Tmp'Address;
+
+    begin
+        return Result;
+    end Get_Message;
 
     -- Example from "Concurrent and Real-Time Programming
     -- in Ada by Alan Burns and Andy Wellings
@@ -43,8 +104,11 @@ package body Common_Tasks is
         use Ada.Real_Time;
         i: Integer := 1;
 
---        Seconds_Since_Epoch : Ada.Real_Time.Seconds_Count;
---        Fractional_Time: Ada.Real_Time.Time_Span;
+        Remote_Address  : GNAT.Sockets.Sock_Addr_Type;
+        Socket          : GNAT.Sockets.Socket_Type;
+        Stream_Access   : GNAT.Sockets.Stream_Access;
+        --Message_Length  : Natural;
+
         Epoch_Time_Seconds: Ada.Real_Time.Seconds_Count;
         Epoch_Time_Fractional_Time: Ada.Real_Time.Time_Span;
 
@@ -65,43 +129,70 @@ package body Common_Tasks is
                                     Ada.Real_Time.To_Duration(
                                         Epoch_Time_Fractional_Time)) & ")");
 
-        --------------------------------------------------------------------
-        -- Do Actual Work
-        --------------------------------------------------------------------
-
-        --------------------------------------------------------------------
-        -- End Actual Work
-        --------------------------------------------------------------------
+        Remote_Address.Addr :=  GNAT.Sockets.Addresses(
+                                      GNAT.Sockets.Get_Host_By_Name(
+                                          DST_ADDRESS), 1);
+        Remote_Address.Port := PORT;
 
         Next_Period := Ada.Real_Time.Clock + Period;
         loop
             delay until Next_Period;
+            -------------------------------------------------------------------
+            -- Do Actual Work
+            -------------------------------------------------------------------
+            GNAT.Sockets.Initialize;
+            GNAT.Sockets.Create_Socket(Socket);
+
+            GNAT.Sockets.Set_Socket_Option(Socket,
+                                            GNAT.Sockets.Socket_Level,
+                                            (GNAT.Sockets.Reuse_address, True));
+
+            GNAT.Sockets.Connect_Socket(Socket, Remote_Address);
+            Stream_Access := GNAT.Sockets.Stream(Socket);
+
+            -- Send first message, Client to Server
+            String'Write (Stream_Access, "This is the first message from Client to Server" & CRLF & CRLF);
+
+            GNAT.Sockets.Close_Socket(Socket);
+            GNAT.Sockets.Finalize;
+            -------------------------------------------------------------------
+            -- End Actual Work
+            -------------------------------------------------------------------
+
             Next_Period := Next_Period + Period;
             i := i + 1;
             exit when i = 6;
         end loop;
         Ada.Text_IO.Put_Line("Task Tcp_Send - End");
-
     end Tcp_Sender_Type;
 
     -- Tcp_Listener_Type
     task body Tcp_Listener_Type is
         use Ada.Real_Time;
+        use type GNAT.Sockets.Selector_Status;
+
         i: Integer := 1;
 
---        Seconds_Since_Epoch : Ada.Real_Time.Seconds_Count;
---        Fractional_Time: Ada.Real_Time.Time_Span;
         Epoch_Time_Seconds: Ada.Real_Time.Seconds_Count;
         Epoch_Time_Fractional_Time: Ada.Real_Time.Time_Span;
 
-        Next_Period : Ada.Real_Time.Time;
---        Period : constant Ada.Real_Time.Time_Span :=
---                            Ada.Real_Time.Microseconds(Cycle_Time);
+        Epoch_Time : Ada.Real_Time.Time;
+
+        Listener_Socket  : GNAT.Sockets.Socket_Type;
+        Listener_Address : constant GNAT.Sockets.Sock_Addr_Type := (
+         Family => GNAT.Sockets.Family_Inet,
+         Addr   => GNAT.Sockets.Inet_Addr ("127.0.0.1"),
+         Port   => 50000
+        );
+        Client_Socket   : GNAT.Sockets.Socket_Type;
+        Client_Address  : GNAT.Sockets.Sock_Addr_Type;
+        Selector_Status : GNAT.Sockets.Selector_Status;
+        Message_Length  : Natural;
     begin
         Ada.Text_IO.Put_Line("Task Tcp_Listener - Start");
-        Epoch.Get_Start_Time(Next_Period);
+        Epoch.Get_Start_Time(Epoch_Time);
 
-        Ada.Real_Time.Split(Next_Period, Epoch_Time_Seconds,
+        Ada.Real_Time.Split(Epoch_Time, Epoch_Time_Seconds,
                                     Epoch_Time_Fractional_Time);
         Ada.Text_IO.Put_Line("Task Tcp_Listener - Epoch:"
                                 & Ada.Real_Time.Seconds_Count'Image(
@@ -114,23 +205,62 @@ package body Common_Tasks is
         --------------------------------------------------------------------
         -- Do Actual Work
         --------------------------------------------------------------------
+        GNAT.Sockets.Create_Socket (Listener_Socket);
+        GNAT.Sockets.Set_Socket_Option (
+            Socket => Listener_Socket,
+            Level  => GNAT.Sockets.Socket_Level,
+            Option => (
+                Name    => GNAT.Sockets.Reuse_Address,
+                Enabled => True
+            )
+        );
 
-        --------------------------------------------------------------------
-        -- End Actual Work
-        --------------------------------------------------------------------
+        GNAT.Sockets.Bind_Socket (
+            Socket  => Listener_Socket,
+            Address => Listener_Address
+        );
 
---        Next_Period := Ada.Real_Time.Clock + Period;
+        GNAT.Sockets.Listen_Socket (Listener_Socket);
+        Ada.Text_IO.Put_Line (
+            "Listening on " & GNAT.Sockets.Image (Listener_Address)
+        );
+
         loop
---            delay until Next_Period;
---            Next_Period := Next_Period + Period;
+            GNAT.Sockets.Accept_Socket (
+                Server   => Listener_Socket,
+                Socket   => Client_Socket,
+                Address  => Client_Address,
+                Timeout  => GNAT.Sockets.Forever,
+                --Selector => S.Accepting_Selector'Access,
+                Status   => Selector_Status
+            );
+
+            if Selector_Status = GNAT.Sockets.Completed then
+                Message_Length := Read_Request(Client_Socket);
+
+                Ada.Text_IO.Put_Line ("DEBUG: Message Length from Read_Request" & Natural'Image(Message_Length));
+                declare
+                    Message : String(1 .. Message_Length) := Get_Message(Message_Length);
+                begin
+                    Ada.Text_IO.Put_Line(Message);
+                end;
+                Ada.Text_IO.Put_Line (
+                    "Client from " & GNAT.Sockets.Image (Client_Address)
+                );
+                GNAT.Sockets.Close_Socket (Client_Socket);
+            end if;
+
             i := i + 1;
             exit when i = 6;
         end loop;
+        --------------------------------------------------------------------
+        -- End Actual Work
+        --------------------------------------------------------------------
+        GNAT.Sockets.Close_Socket (Listener_Socket);
         Ada.Text_IO.Put_Line("Task Tcp_Listener - End");
-
     end Tcp_Listener_Type;
 
-    Tcp_Sender : Tcp_Sender_Type(20, 200, 1, 3);
+    Tcp_Sender : Tcp_Sender_Type(20, 2000000, 1, 3);
     Tcp_Listener: Tcp_Listener_Type(1);
 
 end Common_Tasks;
