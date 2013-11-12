0ee440f5667bca891e823d4456e145213b0bf0b4
Per-Erik Måhl <pml09001@student.mdh.se>
2013-11-12 13:06:28 +0100

    tested orientational

--- src/software/navigation/orientational_controller/obj/gnattest/tests/navigation-orientational_controller-corientationalcontroller_test_data-corientationalcontroller_tests.ads
+++ src/software/navigation/orientational_controller/obj/gnattest/tests/navigation-orientational_controller-corientationalcontroller_test_data-corientationalcontroller_tests.ads
@@ -9,35 +9,38 @@ package Navigation.Orientational_Controller.COrientationalController_Test_Data.C
    type Test_COrientationalController is new
      GNATtest_Generated.GNATtest_Standard.Navigation.Orientational_Controller.COrientationalController_Test_Data.Test_COrientationalController with null record;
 
-   procedure Test_xGet_Orientational_Thruster_Control_Values_ec7316 (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:29:4:xGet_Orientational_Thruster_Control_Values
+   procedure Test_Get_Orientational_Thruster_Control_Values_6c2c02 (Gnattest_T : in out Test_COrientationalController);
+   --  navigation-orientational_controller.ads:36:4:Get_Orientational_Thruster_Control_Values
 
    procedure Test_Update_Current_Errors_a4d64d (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:35:4:Update_Current_Errors
+   --  navigation-orientational_controller.ads:42:4:Update_Current_Errors
 
    procedure Test_Set_New_PID_Component_Scalings_f2c0ab (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:39:4:Set_New_PID_Component_Scalings
+   --  navigation-orientational_controller.ads:46:4:Set_New_PID_Component_Scalings
+
+   procedure Test_fGetCurrentErrors_4d9c3d (Gnattest_T : in out Test_COrientationalController);
+   --  navigation-orientational_controller.ads:53:4:fGetCurrentErrors
 
    procedure Test_Update_Current_X_Rotation_Error_80be14 (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:47:4:Update_Current_X_Rotation_Error
+   --  navigation-orientational_controller.ads:57:4:Update_Current_X_Rotation_Error
 
    procedure Test_Update_Current_Y_Rotation_Error_02960d (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:48:4:Update_Current_Y_Rotation_Error
+   --  navigation-orientational_controller.ads:58:4:Update_Current_Y_Rotation_Error
 
    procedure Test_Update_Current_Z_Rotation_Error_6e9d1b (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:49:4:Update_Current_Z_Rotation_Error
+   --  navigation-orientational_controller.ads:59:4:Update_Current_Z_Rotation_Error
 
    procedure Test_xGet_X_Rotation_Thruster_Control_Value_425dce (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:51:4:xGet_X_Rotation_Thruster_Control_Value
+   --  navigation-orientational_controller.ads:61:4:xGet_X_Rotation_Thruster_Control_Value
 
    procedure Test_xGet_Y_Rotation_Thruster_Control_Value_beea5c (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:52:4:xGet_Y_Rotation_Thruster_Control_Value
+   --  navigation-orientational_controller.ads:62:4:xGet_Y_Rotation_Thruster_Control_Value
 
    procedure Test_xGet_Z_Rotation_Thruster_Control_Value_bdcbef (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:53:4:xGet_Z_Rotation_Thruster_Control_Value
+   --  navigation-orientational_controller.ads:63:4:xGet_Z_Rotation_Thruster_Control_Value
 
    procedure Test_Finalize_1d29f1 (Gnattest_T : in out Test_COrientationalController);
-   --  navigation-orientational_controller.ads:66:4:Finalize
+   --  navigation-orientational_controller.ads:86:4:Finalize
 
 end Navigation.Orientational_Controller.COrientationalController_Test_Data.COrientationalController_Tests;
 --  end read only
