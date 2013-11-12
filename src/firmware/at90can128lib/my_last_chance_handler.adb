0dbe3d15ef35165299ab7f85aef29fa2fed7aa7b
nils1337 <nbl08001@student.mdh.se>
2013-11-12 08:28:21 +0100

    wrote files to make stuff compile

---
+++ src/firmware/at90can128lib/my_last_chance_handler.adb
@@ -0,0 +1,9 @@
+
+package body My_Last_Chance_Handler is
+
+   procedure Last_Chance_Handler(Source_Location : System.Address; Line : Integer) is
+   begin
+      null;
+   end Last_Chance_Handler;
+
+end My_Last_Chance_Handler;
---
+++ src/firmware/at90can128lib/my_memcpy.adb
@@ -0,0 +1,23 @@
+
+--copied from http://docs.adacore.com/gnat-hie-docs/html/gnathie_ug_3.html
+--By Nils
+
+with Ada.Unchecked_Conversion;
+function My_Memcpy (dest, src : Address;
+                 n         : size_t) return Address is
+   subtype mem is char_array (size_t);
+   type memptr is access mem;
+   function to_memptr is
+      new Ada.Unchecked_Conversion (address, memptr);
+   dest_p : constant memptr := to_memptr (dest);
+   src_p  : constant memptr := to_memptr (src);
+
+begin
+  if n > 0 then  -- need to guard against n=0 since size_t is a modular type
+    for J in 0 .. n - 1 loop
+       dest_p (J) := src_p (J);
+    end loop;
+  end if;
+
+  return dest;
+end My_Memcpy;
