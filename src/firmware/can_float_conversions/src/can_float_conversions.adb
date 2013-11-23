440dd69bc2d1b264292f6b13e60e9522bfe8decb
nils1337 <nbl08001@student.mdh.se>
2013-11-23 17:32:41 +0100

    Continued to implement the can_float_conversions project.

---
+++ src/firmware/can_float_conversions/src/can_float_conversions.adb
@@ -0,0 +1,101 @@
+
+---------------------------------------------------------------------------
+-- This code implements functions to put INS-data (float values) into can messages.
+-- Written by Nils Brynedal Ignell for the Naiad AUV project
+-- Last changed (yyyy-mm-dd): 2013-11-23
+
+-- TODO: Unit testing
+---------------------------------------------------------------------------
+
+
+package body Can_Float_Conversions is
+
+   pragma Suppress(All_Checks);
+
+
+   procedure Orientation_To_Message(fYaw : float; fPitch : float; fRoll : float; b8Message : out AVR.AT90CAN128.CAN.Byte8) is
+      Data : TOrientation;
+      fYawInternal 	: float := Float'Max(Float'Min(fYaw,   fYAW_MAX),   -fYAW_MAX);
+      fPitchInternal 	: float := Float'Max(Float'Min(fPitch, fPITCH_MAX), -fPITCH_MAX);
+      fRollInternal 	: float := Float'Max(Float'Min(fRoll,  fROLL_MAX),  -fROLL_MAX);
+   begin
+
+      Data.i21Yaw   := Integer_21(fYawInternal   / fYAW_RESOLUTION);
+      Data.i21Pitch := Integer_21(fPitchInternal / fPITCH_RESOLUTION);
+      Data.i21Roll  := Integer_21(fRollInternal  / fROLL_RESOLUTION);
+
+      b8Message :=  TOrientationToMessage(Data);
+   end Orientation_To_Message;
+
+   procedure Message_To_Orientation(fYaw : out float; fPitch : out float; fRoll : out float; b8Message : AVR.AT90CAN128.CAN.Byte8) is
+      Data : TOrientation;
+   begin
+      Data :=  TMessageToOrientation(b8Message);
+      fYaw   := Float(Data.i21Yaw)   * fYAW_RESOLUTION;
+      fPitch := Float(Data.i21Pitch) * fPITCH_RESOLUTION;
+      fRoll  := Float(Data.i21Roll)  * fROLL_RESOLUTION;
+   end Message_To_Orientation;
+
+
+   -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+   procedure Acceleration_To_Message(fAccX : float; fAccY : float; fAccZ : float; b8Message : out AVR.AT90CAN128.CAN.Byte8) is
+      Data : TOrientation;
+
+      fAccXInternal 	: float := Float'Max(Float'Min(fAccX, fACCELERATION_MAX), -fACCELERATION_MAX); -- if these lines are doing anything to the values, then the robot is doing things it shouldn't be doing (crashing into stuff etc...)
+      fAccYInternal 	: float := Float'Max(Float'Min(fAccY, fACCELERATION_MAX), -fACCELERATION_MAX);
+      fAccZInternal 	: float := Float'Max(Float'Min(fAccZ, fACCELERATION_MAX), -fACCELERATION_MAX);
+   begin
+      -- We are using the same conversion technique as for orientation simply
+      -- because there is no need to do differently...
+      Data.i21Yaw   := Integer_21(fAccXInternal / fACCELERATION_RESOLUTION);
+      Data.i21Pitch := Integer_21(fAccYInternal / fACCELERATION_RESOLUTION);
+      Data.i21Roll  := Integer_21(fAccZInternal / fACCELERATION_RESOLUTION);
+      b8Message :=  TOrientationToMessage(Data);
+   end Acceleration_To_Message;
+
+
+
+   procedure Message_To_Acceleration(b8Message : AVR.AT90CAN128.CAN.Byte8; fAccX : out float; fAccY : out float; fAccZ : out float) is
+      Data : TOrientation;
+   begin
+      -- We are using the same conversion technique as for orientation simply
+      -- because there is no need to do differently...
+      Data :=  TMessageToOrientation(b8Message);
+      fAccX := Float(Data.i21Yaw)   * fACCELERATION_RESOLUTION;
+      fAccY := Float(Data.i21Pitch) * fACCELERATION_RESOLUTION;
+      fAccZ := Float(Data.i21Roll)  * fACCELERATION_RESOLUTION;
+   end Message_To_Acceleration;
+
+   -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+   procedure GyroReading_To_Message(fGyroReading : float; b8Message : out AVR.AT90CAN128.CAN.Byte8) is
+      i24Reading : Integer_24;
+      ReadingArr : TGyroReadingArray;
+      fReading 	: float := Float'Max(Float'Min(fGyroReading, fGYRO_MAX), -fGYRO_MAX); --something is wrong if this value is needed...
+   begin
+
+      i24Reading   := Integer_24(fReading / fGYRO_RESOLUTION);
+      ReadingArr := i24ToGyroReading(i24Reading);
+
+      b8Message(1) := ReadingArr(1);
+      b8Message(2) := ReadingArr(2);
+      b8Message(3) := ReadingArr(3);
+   end GyroReading_To_Message;
+
+
+   procedure Message_To_GyroReading(b8Message : AVR.AT90CAN128.CAN.Byte8; fGyroReading : out float) is
+      i24Reading : Integer_24;
+      ReadingArr : TGyroReadingArray;
+   begin
+      ReadingArr(1) := b8Message(1);
+      ReadingArr(2) := b8Message(2);
+      ReadingArr(3) := b8Message(3);
+
+      i24Reading := GyroReadingToi24(ReadingArr);
+
+      fGyroReading := Float(i24Reading) * fGYRO_RESOLUTION;
+   end Message_To_GyroReading;
+
+end Can_Float_Conversions;
+
---
+++ src/firmware/can_float_conversions/src/can_float_conversions.ads
@@ -0,0 +1,79 @@
+
+---------------------------------------------------------------------------
+-- This code implements functions to put INS-data (float values) into can messages.
+-- Written by Nils Brynedal Ignell for the Naiad AUV project
+-- Last changed (yyyy-mm-dd): 2013-11-23
+
+-- TODO: Unit testing
+--------------------------------------------------------------------------
+
+with Ada.Unchecked_Conversion;
+with Interfaces;
+
+with AVR.AT90CAN128.CAN;
+
+package Can_Float_Conversions is
+
+    --all 8 bytes of b8Message need to be sent
+   procedure Orientation_To_Message(fYaw : float; fPitch : float; fRoll : float; b8Message : out AVR.AT90CAN128.CAN.Byte8);
+   procedure Message_To_Orientation(fYaw : out float; fPitch : out float; fRoll : out float; b8Message : AVR.AT90CAN128.CAN.Byte8);
+
+   --all 8 bytes of b8Message need to be sent
+   procedure Acceleration_To_Message(fAccX : float; fAccY : float; fAccZ : float; b8Message : out AVR.AT90CAN128.CAN.Byte8);
+   procedure Message_To_Acceleration(b8Message : AVR.AT90CAN128.CAN.Byte8; fAccX : out float; fAccY : out float; fAccZ : out float);
+
+   --only the 3 first bytes of b8Message need to be sent
+   procedure GyroReading_To_Message(fGyroReading : float; b8Message : out AVR.AT90CAN128.CAN.Byte8);
+   procedure Message_To_GyroReading(b8Message : AVR.AT90CAN128.CAN.Byte8; fGyroReading : out float);
+
+private
+
+   fYAW_MAX 	: constant float := 180.0;
+   fPITCH_MAX   : constant float := 90.0;
+   fROLL_MAX 	: constant float := 180.0;
+
+   fYAW_RESOLUTION   : constant float  := fYAW_MAX   / Float(2 ** 21);  --angles in degrees
+   fPITCH_RESOLUTION : constant float  := fPITCH_MAX / Float(2 ** 21);
+   fROLL_RESOLUTION  : constant float  := fROLL_MAX   / Float(2 ** 21);
+
+   fACCELERATION_MAX 		: float := 20.0; --acceleration in m/s^2
+   fACCELERATION_RESOLUTION 	: float := fACCELERATION_MAX / Float(2 ** 21);
+
+   fGYRO_MAX 		: float := 180.0; --angle in degrees
+   fGYRO_RESOLUTION 	: float := fGYRO_MAX / Float(2 ** 24);
+
+
+   type Integer_21 is range -2 ** 20 .. 2 ** 20 - 1;
+   for Integer_21'Size use  21;
+
+   type TOrientation is
+      record
+         i21Yaw   : Integer_21;
+         i21Pitch : Integer_21;
+         i21Roll  : Integer_21;
+         bPadding : Boolean;
+      end record;
+
+   for TOrientation use record
+      i21Yaw 	at 0 range 0  .. 20;
+      i21Pitch 	at 0 range 21 .. 41;
+      i21Roll 	at 0 range 42 .. 62;
+      bPadding 	at 0 range 63 .. 63;
+   end record;
+   for TOrientation'Size use 64;
+
+   function TOrientationToMessage is new Ada.Unchecked_Conversion(TOrientation, AVR.AT90CAN128.CAN.Byte8);
+   function TMessageToOrientation is new Ada.Unchecked_Conversion(AVR.AT90CAN128.CAN.Byte8, TOrientation);
+
+   type Integer_24 is range -2 ** 23 .. 2 ** 23 - 1;
+   for Integer_24'Size use 24;
+
+   type TGyroReadingArray is array(1..3) of Interfaces.Unsigned_8;
+   for TGyroReadingArray'Size use  24;
+
+
+   function i24ToGyroReading is new Ada.Unchecked_Conversion(Integer_24, TGyroReadingArray);
+   function GyroReadingToi24 is new Ada.Unchecked_Conversion(TGyroReadingArray, Integer_24);
+
+
+end Can_Float_Conversions;
