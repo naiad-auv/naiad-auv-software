a4baab60173bffe27898e195662a48e3dc106230
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-12-11 08:39:47 +0100

    Example xmlada 001 now reads one argument from command line and opens that file.

--- examples/xmlada/001-xmlada-read_file/src/main.adb
+++ examples/xmlada/001-xmlada-read_file/src/main.adb
@@ -7,40 +7,29 @@ use DOM.Readers;
 with DOM.Core;
 use DOM.Core;
 with Ada.Command_line;
-use Ada.Command_Line;
 -- with Ada.Strings.Unbounded.Text_IO;
 -- use Ada.Strings.Unbounded.Text_IO;
 with Ada.Text_IO;
--- use Ada.Text_IO;
-with Ada.Strings.Bounded;
+use Ada.Text_IO;
+-- with Ada.Strings.Bounded;
 -- use Ada.Strings.Bounded;
 
 procedure main is
-
-    package Strings_Bounded is new Ada.Strings.Bounded.Generic_Bounded_Length(
-                                        Max => 100);
-    use Strings_Bounded;
-
     Input   : File_Input;
     Reader  : Tree_Reader;
     Doc     : Document;
-    Current_Argument : Strings_Bounded.Bounded_String;
-    File_Name : Strings_Bounded.Bounded_String;
+    File_Name : String := Ada.Command_Line.Argument(1);
 begin
-    File_Name := Strings_Bounded.To_Bounded_String(Argument(1));
-    Put(File_Name);
-    Ada.Text_IO.New_Line;
+    Set_Public_Id (Input, "Preferences file");
+    Open (File_Name, Input);
 
---    Set_Public_Id (Input, "Preferences file");
---    Open ("xml/pref.xml", Input);
---
---    Set_Feature (Reader, Validation_Feature, False);
---    Set_Feature (Reader, Namespace_Feature, False);
---
---    Parse (Reader, Input);
---    Close (Input);
---
---    Doc := Get_Tree (Reader);
---
---    Free (Reader);
+    Set_Feature (Reader, Validation_Feature, False);
+    Set_Feature (Reader, Namespace_Feature, False);
+
+    Parse (Reader, Input);
+    Close (Input);
+
+    Doc := Get_Tree (Reader);
+
+    Free (Reader);
 end main;
