4437d5413faa1bc25aafc6d6f4383075aa0d5237
Nils Brynedal <nbl08001@student.mdh.se>
2013-12-12 12:08:01 +0100

    minor changes

--- examples/at90can_adc_test/src/at90can_adc_test.adb
+++ examples/at90can_adc_test/src/at90can_adc_test.adb
@@ -18,30 +18,7 @@ procedure AT90CAN_ADC_test is
    bTemp : Boolean;
    u16Value : Interfaces.Unsigned_16;
 
-   -- sBuffer : String(1..5);
    u8 : Interfaces.Unsigned_8;
-
---     procedure u16ToStr(u16 : Interfaces.Unsigned_16; sRet : out String) is
---        use Interfaces;
---        iDigit : Integer;
---        iLeft : Integer := Integer(u16);
---        iValue : Integer := 1000;
---     begin
---
---        for i in reverse 1..4 loop
---           iDigit := iLeft / iValue;
---           iLeft := iLeft - (iDigit * iValue);
---
---           if iDigit = 0 then
---              sRet(5-i) := ' ';
---           else
---              sRet(5-i) := Character'Val(iDigit + Character'Pos('0'));
---           end if;
---           iValue := iValue / 10;
---        end loop;
---       sRet(5) := Character'Val(0);
---
---     end u16ToStr;
 
 begin
 
